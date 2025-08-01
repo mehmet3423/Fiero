cf1a0e790ae2d98e0f49fc0722ccffe8fa19796f
arigmehmetsait <1200505043@ogrenci.kirklareli.edu.tr>
2025-08-01 10:36:49 +0300

    OrderSupport-fixed

--- src/components/home/HomeCategories.tsx
+++ src/components/home/HomeCategories.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SORT_OPTIONS } from "@/constants/enums/SortOptions";
 
 const HomeCategories: React.FC = () => {
   const categories = [
-    { title: "New Arrivals", img: "/assets/site/images/collections/collection-circle-8.jpg", link: "/shop-collection-sub" },
-    { title: "Best Sellers", img: "/assets/site/images/collections/collection-circle-9.jpg", link: "/shop-collection-sub" },
-    { title: "Top Rated", img: "/assets/site/images/collections/collection-circle-10.jpg", link: "/shop-collection-sub" },
-    { title: "Brands We Love", img: "/assets/site/images/collections/collection-circle-11.jpg", link: "/shop-collection-sub" },
-    { title: "Trending", img: "/assets/site/images/collections/collection-circle-12.jpg", link: "/shop-collection-sub" },
-    { title: "The Re-Imagined", img: "/assets/site/images/collections/collection-circle-13.jpg", link: "/shop-collection-sub" },
-    { title: "Sale", img: "/assets/site/images/collections/sale.jpg", link: "/shop-collection-sub", sale: "30% off" },
+    {
+      title: "New Arrivals",
+      img: "/assets/site/images/collections/collection-circle-8.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.POPULARITY, // Yeni ürünler için popülerlik sıralaması
+    },
+    {
+      title: "Best Sellers",
+      img: "/assets/site/images/collections/collection-circle-9.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.SALES_HIGH, // En çok satanlar
+    },
+    {
+      title: "Top Rated",
+      img: "/assets/site/images/collections/collection-circle-10.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.RATING, // En iyi puanlı ürünler
+    },
+    {
+      title: "Brands We Love",
+      img: "/assets/site/images/collections/collection-circle-11.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.LIKES_HIGH, // En çok beğenilenler
+    },
+    {
+      title: "Trending",
+      img: "/assets/site/images/collections/collection-circle-12.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.POPULARITY, // Trend olanlar için popülerlik
+    },
+    {
+      title: "The Re-Imagined",
+      img: "/assets/site/images/collections/collection-circle-13.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.POPULARITY, // Yeniden tasarlananlar için popülerlik
+    },
+    {
+      title: "Sale",
+      img: "/assets/site/images/collections/sale.jpg",
+      link: "/products",
+      sortBy: SORT_OPTIONS.PRICE_LOW, // İndirimli ürünler için düşük fiyat
+      sale: "30% off",
+    },
   ];
 
   return (
@@ -24,28 +61,37 @@ const HomeCategories: React.FC = () => {
                   <div key={index} className="collection-item-circle hover-img">
                     {category.sale ? (
                       <div className="has-saleoff-wrap position-relative">
-                        <Link href={category.link} className="collection-image img-style">
-                          <img 
-                            className="lazyload" 
-                            data-src={category.img} 
-                            src={category.img} 
-                            alt="collection-img" 
+                        <Link
+                          href={`${category.link}?sortBy=${category.sortBy}`}
+                          className="collection-image img-style"
+                        >
+                          <img
+                            className="lazyload"
+                            data-src={category.img}
+                            src={category.img}
+                            alt="collection-img"
                           />
                         </Link>
                         <div className="sale-off fw-5">{category.sale}</div>
                       </div>
                     ) : (
-                      <Link href={category.link} className="collection-image img-style">
-                        <img 
-                          className="lazyload" 
-                          data-src={category.img} 
-                          src={category.img} 
-                          alt="collection-img" 
+                      <Link
+                        href={`${category.link}?sortBy=${category.sortBy}`}
+                        className="collection-image img-style"
+                      >
+                        <img
+                          className="lazyload"
+                          data-src={category.img}
+                          src={category.img}
+                          alt="collection-img"
                         />
                       </Link>
                     )}
                     <div className="collection-content text-center">
-                      <Link href={category.link} className="link title fw-6">
+                      <Link
+                        href={`${category.link}?sortBy=${category.sortBy}`}
+                        className="link title fw-6"
+                      >
                         {category.title}
                       </Link>
                     </div>
@@ -54,11 +100,14 @@ const HomeCategories: React.FC = () => {
               </div>
               <div className="tf-shopall-wrap">
                 <div className="collection-item-circle tf-shopall">
-                  <Link href="/shop-collection-sub" className="collection-image img-style tf-shopall-icon">
+                  <Link
+                    href="/products"
+                    className="collection-image img-style tf-shopall-icon"
+                  >
                     <i className="icon icon-arrow1-top-left"></i>
                   </Link>
                   <div className="collection-content text-center">
-                    <Link href="/shop-collection-sub" className="link title fw-6">
+                    <Link href="/products" className="link title fw-6">
                       Shop all
                     </Link>
                   </div>
